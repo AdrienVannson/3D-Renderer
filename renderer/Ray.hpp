6b53550e86e001617abb7b66fdbd2dcb3765421c
Adrien VANNSON <adrien.vannson@protonmail.com>
2020-03-15 11:47:22 +0100

    Ray

--- renderer/Ray.hpp
+++ renderer/Ray.hpp
@@ -9,11 +9,26 @@ class Ray
 public:
     Ray (const Vect &pos=Vect(), const Vect &dir=Vect());
 
-    inline Vect pos () const { return m_pos; }
-    inline void setPos (const Vect pos) { m_pos = pos; }
+    inline Vect pos () const
+    {
+        return m_pos;
+    }
 
-    inline Vect dir () const { return m_dir; }
-    inline void setDir (const Vect dir) { m_dir = dir; }
+    inline void setPos (const Vect pos)
+    {
+        m_pos = pos;
+    }
+
+    inline Vect dir () const
+    {
+        return m_dir;
+    }
+
+    inline void setDir (const Vect dir)
+    {
+        m_dir = dir;
+        m_dir.normalize();
+    }
 
     void moveByEpsilon ();
 
