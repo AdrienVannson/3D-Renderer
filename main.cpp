71397b1a97ce48be4e2dccfa94c607baf787a447
Adrien VANNSON <adrien.vannson@protonmail.com>
2020-05-30 17:24:13 +0200

    Show total time

--- main.cpp
+++ main.cpp
@@ -15,24 +15,36 @@
 int main (int argc, char *argv[])
 {
     Stats::init();
+    const clock_t startTime = clock();
 
     // (2) Update to change the scene
     Image *image = generateImageTeapot();
 
-    // Show stats
-    std::cout.imbue(std::locale("")); // Pretty print numbers
+    const clock_t endTime = clock();
 
+    // Show stats
+    const int width1 = 40;
+    const int width2 = 16;
+
+    std::cout.imbue(std::locale("")); // Pretty print numbers
     std::cout << "##### Statistics\n";
-    std::cout << std::left  << std::setw(40) << "Ray-triangle tests count:"
-              << std::right << std::setw(16) << Stats::rayTriangleTestsCount() << "\n";
-    std::cout << std::left  << std::setw(40) << "Ray-triangle intersections count:"
-              << std::right << std::setw(16) << Stats::rayTriangleIntersectionsCount() << "\n";
-    std::cout << std::left  << std::setw(40) << "Ray-box tests count:"
-              << std::right << std::setw(16) << Stats::rayBoxTestsCount() << "\n";
-    std::cout << std::left  << std::setw(40) << "Ray-box intersections count:"
-              << std::right << std::setw(16) << Stats::rayBoxIntersectionsCount() << "\n";
+
+    std::cout << std::left  << std::setw(width1) << "Total CPU time:"
+              << std::right << std::setw(width2-2) << (double)(endTime-startTime) / CLOCKS_PER_SEC << " s\n";
+
+    std::cout << std::left  << std::setw(width1) << "Ray-triangle tests count:"
+              << std::right << std::setw(width2) << Stats::rayTriangleTestsCount() << "\n";
+    std::cout << std::left  << std::setw(width1) << "Ray-triangle intersections count:"
+              << std::right << std::setw(width2) << Stats::rayTriangleIntersectionsCount() << "\n";
+
+    std::cout << std::left  << std::setw(width1) << "Ray-box tests count:"
+              << std::right << std::setw(width2) << Stats::rayBoxTestsCount() << "\n";
+    std::cout << std::left  << std::setw(width1) << "Ray-box intersections count:"
+              << std::right << std::setw(width2) << Stats::rayBoxIntersectionsCount() << "\n";
+
     std::cout.flush();
 
+    // UI
     QApplication a(argc, argv);
 
     // Convert to QImage
